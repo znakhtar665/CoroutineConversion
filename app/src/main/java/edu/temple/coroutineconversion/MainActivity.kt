6f7a347333ef375c839553c41e4c84f7a2266721
znakhtar665 <znakhtar665@gmail.com>
2024-10-31 15:14:11 -0400

    Coroutines Submission

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,11 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,24 +26,35 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+//    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+//
+//        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
+//        cakeImageView.alpha = it.what / 100f
+//        true
+//    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+//            Thread{
+//                repeat(100) {
+//                    handler.sendEmptyMessage(it)
+//                    Thread.sleep(1000)
+//                }
+//            }.start()
+            CoroutineScope(Dispatchers.Main).launch {
+                for (i in 0..100) {
+                 updateOpacity(i)
+                 delay(40)
                 }
-            }.start()
+            }
         }
+    }
+
+    private fun updateOpacity(opacity: Int) {
+        cakeImageView.alpha = opacity / 100f
+        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", opacity)
     }
 }
